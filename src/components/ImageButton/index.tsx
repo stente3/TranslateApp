dc0d536154c34cb9bbf0ec8b7aa41c102a011f47
Stente3 <stentem0103@gmail.com>
2023-12-13 07:49:53 -0500

    Add ImageCenterButton component
    Create a reusable React component, ImageButton, that accepts an imageUrl and altText as a prop to dynamically set the centered image
    This generic button can be easily integrated into various contexts across the project

---
+++ src/components/ImageButton/index.tsx
@@ -0,0 +1,14 @@
+interface ImageTextComponentProps {
+	image: string;
+	altText: string;
+}
+
+export const ImageButton = ({ image, altText }: ImageTextComponentProps) => {
+	return (
+		<>
+			<button className='border-[#4D5562] rounded-md border-2 p-1'>
+				<img src={image} alt={altText} />
+			</button>
+		</>
+	);
+};
